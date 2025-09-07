1e42fcabe05e37c73aefd3cfb96137b0c53a2d74
Steven Scott <codingismy11to7@gmail.com>
2025-09-07 00:26:47 -0400

    missed the x86 version before or it wasn't up yet

--- run_onchange_install_zellij.fish
+++ run_onchange_install_zellij.fish
@@ -1,11 +1,8 @@
 #!/usr/bin/env fish
 
-switch (arch)
-  case aarch64
-    set URL https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-aarch64-unknown-linux-musl.tar.gz
-  case '*'
-    set URL https://github.com/zellij-org/zellij/releases/download/v0.43.0/zellij-x86_64-unknown-linux-musl.tar.gz   
-end
+set VERSION 0.43.1
+
+set URL https://github.com/zellij-org/zellij/releases/download/v{$VERSION}/zellij-(arch)-unknown-linux-musl.tar.gz
 
 echo "Downloading Zellij..."
 
