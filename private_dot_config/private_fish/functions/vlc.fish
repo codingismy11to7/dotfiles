5c8da6e5a784e59b2329d641f8ab6648d882636c
Steven Scott <codingismy11to7@gmail.com>
2025-09-13 23:49:27 -0400

    add vlc alias with makemkv support

---
+++ private_dot_config/private_fish/functions/vlc.fish
@@ -0,0 +1,4 @@
+function vlc --description 'alias vlc LD_LIBRARY_PATH=/var/lib/flatpak/app/com.makemkv.MakeMKV/current/active/files/lib vlc '
+  LD_LIBRARY_PATH=/var/lib/flatpak/app/com.makemkv.MakeMKV/current/active/files/lib /usr/bin/vlc  $argv
+        
+end
