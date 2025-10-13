5d63e2e68eb037866e31c062223887c7416241d6
Steven Scott <steven.scott@codemettle.com>
2025-10-13 10:39:58 -0400

    lazygit isn't oob on ubuntu
    also put btop in work profile
    oh yeah need to replace sourcegit with lazygit

--- run_onchange_install_zellij.fish
+++ run_onchange_install_zellij.fish
@@ -4,12 +4,12 @@ set VERSION 0.43.1
 
 set URL https://github.com/zellij-org/zellij/releases/download/v{$VERSION}/zellij-(arch)-unknown-linux-musl.tar.gz
 
-echo "Downloading Zellij..."
+echo "Downloading Zellij v$VERSION..."
 
 cd (mktemp -d)
 
 curl -L -o ./z.tgz $URL
-tar zxvf ./z.tgz
+tar zxf ./z.tgz
 mv zellij ~/bin
 chmod +x ~/bin/zellij
 
