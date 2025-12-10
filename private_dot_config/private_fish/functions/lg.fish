0117d64d304d915ba2f7843ec8af2e306e2a1101
Steven Scott <steven.scott@codemettle.com>
2025-12-10 09:16:22 -0500

    lg config

--- private_dot_config/private_fish/functions/lg.fish
+++ private_dot_config/private_fish/functions/lg.fish
@@ -1,16 +1,3 @@
 function lg --wraps=lazygit --description 'alias lg lazygit'
-  set -l cwd_file ~/.config/lazygit/cwd
-
-  env LAZYGIT_NEW_DIR_FILE=$cwd_file lazygit $argv
-
-  if test -f $cwd_file
-    set --local target_dir (cat $cwd_file)
-
-    if test -d $target_dir
-      cd $target_dir
-    end
-
-    rm -f $cwd_file
-  end
-
+    lazygit $argv
 end
