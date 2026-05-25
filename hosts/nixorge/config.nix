63d39274e09793e6b29920e8e6881b0b4e62ca4c
Steven Scott <codingismy11to7@gmail.com>
2026-05-25 16:56:39 -0400

    attempt to get hdr working

--- hosts/nixorge/config.nix
+++ hosts/nixorge/config.nix
@@ -7,7 +7,12 @@
     media.sensitiveVolume = true;
     hyprland.monitorConfig = ''
       env = GDK_SCALE,1.5
+      monitor = DP-2, 5120x2160@165, 0x0, 1.6, bitdepth, 10, cm, hdr, sdrbrightness, 1.35
       monitor=,preferred,auto,1.6,bitdepth,10
+
+      render {
+          cm_fs_passthrough = 1
+      }
     '';
   };
 
