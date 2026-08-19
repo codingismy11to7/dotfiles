09ea7505755a50b972458b1a75409345a0e6d1fa
Steven Scott <steven@codemettle.com>
2026-08-19 13:02:09 -0400

    update claude code

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
     claude-code-nix = {
       url = "github:sadjow/claude-code-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.systems.follows = "systems";
     };
 
     codex-cli-nix = {
